97ffd0333a1350bffe86b307db0e07ff5be947e7
gilteixeira <gilteixeira@ua.pt>
2022-04-06 14:58:55 +0000

    building is done in the build folder

--- General Problems/single-threaded/CompileAndRun.sh
+++
@@ -1,3 +0,0 @@
-cmake . && make &&
-./ex1 ../countWords/text0.txt ../countWords/text1.txt ../countWords/text2.txt ../countWords/text3.txt ../countWords/text4.txt #&&
-#./ex2 ../computeDet/mat128_32.bin #../computeDet/mat128_64.bin ../computeDet/mat128_128.bin ../computeDet/mat128_256.bin takes a loooong time!
